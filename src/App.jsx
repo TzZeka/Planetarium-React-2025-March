ddbdf91d1c5eeffd7a25dc389c718121b394479b
Tsvetomir <1hundredheads@gmail.com>
2025-03-17 00:04:12 +0200

    Created react app and prapare the files for my project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import StarryBackground from "./components/Background/StarryBackground";
+
+
+function App() {
+  return (
+    <div className="App"> 
+      <StarryBackground/>
+      
+    </div>
+  );
+}
+
+export default App;
