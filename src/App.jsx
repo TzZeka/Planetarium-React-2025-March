43b3e3916b9f9c45480d3028bbd0f0dab46a0adb
Tsvetomir <1hundredheads@gmail.com>
2025-03-19 01:03:15 +0200

    Home , Header, components are added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import StarryBackground from "./components/Background/StarryBackground";
-
+import Header from "./components/Header/header";
+import Home from "./components/Home/home";
 
 function App() {
   return (
-    <div className="App"> 
+    <div className="App">
       <StarryBackground/>
-      
+      <Header/>
+      <Home/>
+
     </div>
   );
 }
