0cbbda530a2a2b085b198c02a692ed80a65ca015
Tsvetomir <1hundredheads@gmail.com>
2025-03-19 01:09:32 +0200

    Home page created

---
+++ src/components/Home/home.jsx
@@ -0,0 +1,10 @@
+import './home.css'; 
+
+export default function Home() {
+  return (
+    <div className="starry-background">
+        <h1 className="title">Planetarium</h1>
+        <p className="description">Welcome to the Planetarium</p>
+    </div>
+  );
+}
