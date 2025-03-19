41ae53c6e9c9b2d11c7a888251ec74acacf1e416
Tsvetomir <1hundredheads@gmail.com>
2025-03-19 19:57:18 +0200

    Structure of components updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+
+import {Routes, Route, } from 'react-router';
+
+
 import StarryBackground from "./components/Background/StarryBackground";
 import Header from "./components/Header/header";
-import Home from "./components/Home/home";
+import Home from "./pages/Home/home";
+import About from './pages/About/about';
+import Contacts from './pages/Contacts/contacts';
 
 function App() {
+
+  
+
   return (
     <div className="App">
       <StarryBackground/>
       <Header/>
-      <Home/>
-
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contacts" element={<Contacts/>} />
+        <Route path="/planets" element={<h1>Planets</h1>} />
+      </Routes>
     </div>
   );
 }
