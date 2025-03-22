a07e69095b9f60f36b0acf5357dfaf61591bd3b3
Tsvetomir <1hundredheads@gmail.com>
2025-03-22 22:30:25 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-
 import './App.css';
 import StarryBackground from "./components/Background/StarryBackground";
 import Header from "./components/Header/header";
-import AppRoutes from "./routes/AppRoutes"; // Импортираме компонентa с маршрутите
+import AppRoutes from "./routes/AppRoutes"; 
 import { AuthProvider } from "./contexts/AuthContext";
 
 
 
 function App() {
   return (
-    <AuthProvider>
+       
     <div className="App">
-      
-        <StarryBackground />
-        <Header />
+      <StarryBackground />
+     
+       <AuthProvider>
+        <Header /> {/* Използваме компонента Header */}
         <AppRoutes /> {/* Използваме дефинираните маршрути */}
-      
+        </AuthProvider>
     </div>
-    </AuthProvider>
+   
   );
 }
 
