f19d127eb25e1cf9c5c2017f25de38ac5ba65486
Tsvetomir <1hundredheads@gmail.com>
2025-03-30 01:56:16 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router'
 
 import App from './App.jsx'
-import { AuthProvider } from './contexts/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
-    <AuthProvider>
     <App />
-    </AuthProvider>
 </BrowserRouter>
 )
