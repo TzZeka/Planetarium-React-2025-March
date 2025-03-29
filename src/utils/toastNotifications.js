d3d714fc5443d0002094cecdf92e28944f2848d7
Tsvetomir <1hundredheads@gmail.com>
2025-03-29 22:50:42 +0200

    Created toastSuccess, toastError, and toastInfo functions for streamlined notification management.

---
+++ src/utils/toastNotifications.js
@@ -0,0 +1,54 @@
+// toastNotifications.js
+
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+/**
+ * 
+ * @param {string} message - Съобщение, което ще се покаже
+ */
+export const toastSuccess = (message) => {
+  toast.success(message, {
+    position: "top-right", 
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true, 
+    pauseOnHover: true,
+    draggable: true, 
+    progress: undefined,
+    style: { backgroundColor: "#d4edda", color: "#155724" },
+  });
+};
+
+/**
+ * 
+ * @param {string} message - 
+ */
+export const toastError = (message) => {
+  toast.error(message, {
+    position: "top-right", 
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    style: { backgroundColor: "#f8d7da", color: "#721c24" }, 
+  });
+};
+
+/**
+ * 
+ * @param {string} message - 
+ */
+export const toastInfo = (message) => {
+  toast.info(message, {
+    position: "bottom-right",
+    autoClose: 4000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: false,
+    style: { backgroundColor: "#d1ecf1", color: "#0c5460" },
+  });
+};
