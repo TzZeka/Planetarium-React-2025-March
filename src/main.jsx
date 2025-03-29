be19cb413004b4e308309eea26249be1a25a6b52
Tsvetomir <1hundredheads@gmail.com>
2025-03-29 23:07:54 +0200

    AuthProvider wraped around the App component

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ BrowserRouter,
 } from 'react-router'
 
 import App from './App.jsx'
+import { AuthProvider } from './contexts/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
+    <AuthProvider>
     <App />
+    </AuthProvider>
 </BrowserRouter>
 )
