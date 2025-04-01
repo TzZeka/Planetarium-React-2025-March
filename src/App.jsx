745ce37c5d1ccca59b265c832c67745ad7fb99d0
Tsvetomir <1hundredheads@gmail.com>
2025-04-01 14:43:26 +0300

    ErrorBoundary added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AppRoutes from "./routes/AppRoutes";
 import { ToastContainer } from "react-toastify";
 import Footer from "./components/Footer/footer";
 import { AuthProvider } from "./contexts/AuthContext";
+import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
 
 
 
@@ -13,11 +14,13 @@ function App() {
   return (
      <AuthProvider> 
         <div className="App">
-          <ToastContainer/>
-          <StarryBackground />
-          <Header />
-          <AppRoutes />
-          <Footer/>         
+            <ToastContainer/>
+            <StarryBackground />
+          <ErrorBoundary>
+              <Header />
+              <AppRoutes />
+          </ErrorBoundary>
+            <Footer/>         
         </div>
     </AuthProvider> 
    
